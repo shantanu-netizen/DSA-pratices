24c5376762206337fbae4e0f9fa3cd27ce1e0866
golu914 <shantanupratap58@gmail.com>
2026-07-23 15:57:22 +0530

    Leetcode

--- leetcodeMaths/problem1-10.js
+++ leetcodeMaths/problem1-10.js
@@ -79,3 +79,17 @@ function isPrime(num) {
   }
   return true;
 }
+// Leetcode 1295. Find Numbers with Even Number of Digits
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let count=0
+    for(let num of nums){
+        if(num.toString().length%2==0){
+            count++
+        }
+    }
+    return count
+};
