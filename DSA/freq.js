27eaf250d10a04f7e9267fc789c5ad368cf65350
golu914 <shantanupratap58@gmail.com>
2026-08-16 13:47:18 +0530

    String

--- DSA/freq.js
+++ DSA/freq.js
@@ -36,3 +36,48 @@ var findDisappearedNumbers = function(nums) {
     }
     return res
 };
+//2965. Find Missing and Repeated Values
+/**
+ * @param {number[][]} grid
+ * @return {number[]}
+ */
+var findMissingAndRepeatedValues = function(grid) {
+    let n=grid.length
+    let freq=new Array(n*n+1).fill(0)
+    let res=[]
+    for(let row of grid){
+        for(let num of row){
+            freq[num]++
+        }
+    }
+    for(let i=1;i<freq.length;i++){
+        if(freq[i]==2){
+            res[0]=i
+        }if(freq[i]==0){
+            res[1]=i
+        }
+    }
+    return res
+};
+//645. Set Mismatch
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    let n=nums.length
+    let freq=new Array(n+1).fill(0)
+    let res=[]
+    for(let val of nums){
+        freq[val]++
+    }
+    for(let i=1;i<freq.length;i++){
+        if(freq[i]==2){
+            res[0]=i
+        }
+        if(freq[i]==0){
+            res[1]=i
+        }
+    }
+    return res
+};
---
+++ string/Leetcode.js
@@ -0,0 +1,78 @@
+
+//58. Length of Last Word
+var lengthOfLastWord = function (s) {
+  let arr = s.trim().split(" ");
+  return arr[arr.length - 1].length;
+};
+/*
+//another solution
+function length(s) {
+let s = s.trim()
+let count = 0
+for(let i = s.length - 1; i >= 0; i--) {
+        if (s.charAt(i) != ' ') {
+        count++
+        } else {
+            break
+        }
+    }
+    return count
+}
+*/
+//1790. Check if One String Swap Can Make Strings Equal
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var areAlmostEqual = function(s1, s2) {
+     let mismatch = new Array(2);
+    let count = 0;
+
+    // Find mismatches
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i]) {
+            count++;
+
+            if (count > 2) {
+                return false;
+            }
+
+            mismatch[count - 1] = i;
+        }
+    }
+
+    // If no mismatch, strings are already equal
+    if (count === 0) {
+        return true;
+    }
+
+    // Check cross matching
+    return s1[mismatch[0]] === s2[mismatch[1]] &&
+           s1[mismatch[1]] === s2[mismatch[0]];
+};
+
+//13. Roman to Integer
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let map=new Map()
+    map.set('I', 1);
+    map.set('V', 5);
+    map.set('X', 10);
+    map.set('L', 50);
+    map.set('C', 100);
+    map.set('D', 500);
+    map.set('M', 1000);
+    let res=0
+    for(let i=0;i<s.length;i++){
+        if(i<s.length-1&&map.get(s[i])<map.get(s[i+1])){
+            res=res-map.get(s[i])
+        }else{
+            res=res+map.get(s[i])
+        }
+    }
+    return res
+};
